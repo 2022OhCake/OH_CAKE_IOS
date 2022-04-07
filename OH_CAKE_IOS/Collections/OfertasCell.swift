25731a0d8220ca494093d40e57e69b8ed3262ae5
josem700 <jgarciajosemanuel@gmail.com>
2022-04-07 15:04:02 +0200

    Pulidas las vistas

---
+++ OH_CAKE_IOS/Collections/OfertasCell.swift
@@ -0,0 +1,15 @@
+//
+//  OfertasCell.swift
+//  OH_CAKE_IOS
+//
+//  Created by Jose Manuel Qastusoft on 7/4/22.
+//
+
+import UIKit
+
+class OfertasCell: UICollectionViewCell {
+    
+    override class func awakeFromNib() {
+        //Do things
+    }
+}
