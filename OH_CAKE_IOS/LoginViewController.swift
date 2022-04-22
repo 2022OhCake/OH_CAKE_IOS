f1d00bf0bd55506504bef76329a9495209f02ea4
josem700 <jgarciajosemanuel@gmail.com>
2022-04-22 11:09:53 +0200

    Pequeño avance y creado viewcontroller de login

---
+++ OH_CAKE_IOS/LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  OH_CAKE_IOS
+//
+//  Created by Jose Manuel Qastusoft on 22/4/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var PassText: UITextField!
+    @IBOutlet weak var UserText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func EnviarButton(_ sender: Any) {
+        
+        if UserText.text!.isEmpty{
+            let alert = UIAlertController(title: "Error", message: "Error, el campo usuario no puede estar vacio", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+        }
+        
+        else if PassText.text!.isEmpty{
+            let alert = UIAlertController(title: "Error", message: "Error, el campo contraseña no puede estar vacio", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+        }
+        
+        else{
+            //Hacer aqui la peticion al servidor
+        }
+    }
+    
+
+}
