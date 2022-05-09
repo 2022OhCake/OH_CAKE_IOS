3c72eec82d5e5d29ef25c47a81eff44eb1db9b82
josem700 <jgarciajosemanuel@gmail.com>
2022-05-09 13:58:47 +0200

    Empezadas Categorias

--- OH_CAKE_IOS/Collections/Categorias/CategoriasViewController.swift
+++ OH_CAKE_IOS/Collections/Categorias/CategoriasViewController.swift
@@ -7,23 +7,27 @@
 
 import UIKit
 
-class CategoriasViewController: UIViewController {
-
+class CategoriasViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    let categorias = [""]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "celdaCategorias", for: indexPath) as! CategoriasCell
+        
+        return cell
+    }
+    
+    
+    
+    
 
 }
